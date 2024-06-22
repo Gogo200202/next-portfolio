a494e7b560b2aeabbdc69784d359ab8d7d1e8aa6
Gogo200202 <gosheto420@gmail.com>
2024-06-22 16:59:58 +0300

    add footer and nave bar

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,42 +1,36 @@
 import React from "react";
 import Link from "next/link";
 
-
 export default function Navbar () {
+let buttonCssName="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+
   return (
     <>
-    <div>
-      <div className="w-full h-20  bg-blue-950 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            
-            <ul className="hidden md:flex gap-x-6 text-white">
+   
+     <header className="relative h-14">
+     <div className="fixed top-0 left-0 md:flex gap-x-6 p-4">My portfolio </div>
+            <ul className=" fixed top-0 right-0 md:flex gap-x-6  p-4">
+              
             <li>
                 <Link href="/">
-                  <p>Home</p>
+                  <p className={buttonCssName}>Home</p>
                 </Link>
               </li>
               <li>
                 <Link href="/About">
-                  <p>About me</p>
+                  <p className={buttonCssName}>About me</p>
                 </Link>
               </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
+              
               <li>
                 <Link href="/ContactMe">
-                  <p>Contacts me</p>
+                  <p className={buttonCssName}>Contacts me</p>
                 </Link>
               </li>
             </ul>
             
-          </div>
-        </div>
-      </div>
-    </div>
+          </header>
+  
     </>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import  Navigation from "./components/Navigation"
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
   
-      <body className={inter.className}>
+      <body className={inter.className} >
         
       <Navigation></Navigation>
-        
-        {children}</body>
+      <main>
+        {children}
+        </main>
+        <Footer></Footer>
+        </body>
      
     </html>
   );
